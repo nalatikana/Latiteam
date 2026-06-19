b2d635be3637936967f0e8cda8e89d67edc1bd58
nalatikana <nalatikan.a@gmail.com>
2026-06-19 19:48:53 +0700

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 window.LATITEAM_CLOUD_CONFIG = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbxiQxGKpySD7gqrmOQHQzYA0y2Yu9w4cD-gRRxhWxP0iHSE0-ccbmJnh6prGAbQ3aIz/exec",
   autoLoad: true,
   autoSave: true
 };
