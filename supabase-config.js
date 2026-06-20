58497c9b72d756f79827c8e67f6b8ec540336e95
nalatikana <nalatikan.a@gmail.com>
2026-06-20 08:37:44 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.LATITEAM_SUPABASE_CONFIG = {
-  url: "https://epcltwmpkbulsfswoqnt.supabase.co/rest/v1/",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwY2x0d21wa2J1bHNmc3dvcW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NjYxNTIsImV4cCI6MjA5NzQ0MjE1Mn0.EOY6oW9B0o6uYxYO2cC2Rh11cK9ySxu_G6sb8sdIBpc",
+  url: "https://epcltwmpkbulsfswoqnt.supabase.co",
+  anonKey: "sb_publishable_CHebz3SnhCVDGfRmA3oyIA_os3K4Wf6",
   usernameDomain: "latiteam.local",
   autoSave: true
 };
